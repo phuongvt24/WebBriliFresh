5a32a9b4d1c8de1690dd828d201957673de67f77
quynhchi <doquynhchi1311@gmail.com>
2022-11-27 23:05:56 +0800

    Change display names

--- WebBriliFresh/WebBriliFresh/Models/DiscountOrder.cs
+++ WebBriliFresh/WebBriliFresh/Models/DiscountOrder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebBriliFresh.Models;
@@ -8,19 +9,26 @@ public partial class DiscountOrder
 {
     public int DisId { get; set; }
 
+    [DisplayName("Mã Giảm Giá")]
     public string? DisCode { get; set; }
 
+    [DisplayName("Tỉ Lệ Giảm")]
     public double? DisRate { get; set; }
 
+    [DisplayName("Tối Đa")]
     public decimal? MaxDis { get; set; }
 
+    [DisplayName("Bắt Đầu")]
     public DateTime? StartDate { get; set; }
 
+    [DisplayName("Kết Thúc")]
     public DateTime? EndDate { get; set; }
 
-    [Range(1,3)]
+    [Range(1,3, ErrorMessage ="Loại KH phải từ 1-3")]
+    [DisplayName("Loại KH")]
     public int? CusType { get; set; }
 
+    [DisplayName("Trạng Thái")]
     public bool? Status { get; set; }
 
     public virtual ICollection<Order> Orders { get; } = new List<Order>();
