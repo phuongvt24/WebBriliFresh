259b457bec43165fe159e5abdea45a8e29436552
quynhchi <doquynhchi1311@gmail.com>
2022-11-29 02:43:13 +0800

    Fix bug in VerifyDisCode() and change display labels to Vietnamese

--- WebBriliFresh/WebBriliFresh/Models/DiscountOrder.cs
+++ WebBriliFresh/WebBriliFresh/Models/DiscountOrder.cs
@@ -10,32 +10,40 @@ namespace WebBriliFresh.Models;
 public partial class DiscountOrder
 {
     [NotMapped]
-    public string? InitialDiscode { get; set; }
+    public string? InitialDisCode { get; set; }
 
     public int DisId { get; set; }
 
-    [Remote(action: "VerifyDisCode", controller: "DiscountOrders", AdditionalFields = nameof(InitialDiscode), ErrorMessage = "Mã đã tồn tại!")]
+    [Required(ErrorMessage ="Vui lòng điền đầy đủ thông tin")]
+    [Remote(action: "VerifyDisCode", controller: "DiscountOrders", AdditionalFields = "InitialDisCode", ErrorMessage = "Mã đã tồn tại!")]
     [DisplayName("Mã Giảm Giá")]
     public string? DisCode { get; set; }
 
+    [Range(0.05, 1, ErrorMessage = "Tỉ lệ giảm phải từ 0.05 đén 1")]
+    [Required(ErrorMessage = "Vui lòng điền đầy đủ thông tin")]
     [DisplayName("Tỉ Lệ Giảm")]
     public double? DisRate { get; set; }
 
+    [Required(ErrorMessage = "Vui lòng điền đầy đủ thông tin")]
     [DisplayName("Tối Đa")]
     public decimal? MaxDis { get; set; }
 
+    [Required(ErrorMessage = "Vui lòng điền đầy đủ thông tin")]
     [Remote(action: "VerifyDate", controller: "DiscountOrders", AdditionalFields = nameof(EndDate), ErrorMessage = "Không thể kết thúc trước khi bắt đầu!")]
     [DisplayName("Bắt Đầu")]
     public DateTime? StartDate { get; set; }
 
+    [Required(ErrorMessage = "Vui lòng điền đầy đủ thông tin")]
     [Remote(action: "VerifyDate", controller: "DiscountOrders", AdditionalFields = nameof(StartDate), ErrorMessage ="Không thể kết thúc trước khi bắt đầu!")]
     [DisplayName("Kết Thúc")]
     public DateTime? EndDate { get; set; }
 
+    [Required(ErrorMessage = "Vui lòng điền đầy đủ thông tin")]
     [Range(1,3, ErrorMessage ="Loại KH phải từ 1-3")]
     [DisplayName("Loại KH")]
     public int? CusType { get; set; }
 
+    [Required(ErrorMessage = "Vui lòng điền đầy đủ thông tin")]
     [DisplayName("Trạng Thái")]
     public bool? Status { get; set; } = false;
 
