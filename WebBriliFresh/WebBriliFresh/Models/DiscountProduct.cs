cbc81675d921532656be4102e5fceff708114bd5
quynhchi <doquynhchi1311@gmail.com>
2022-11-30 01:53:53 +0800

    Scaffold CRUD for DiscountProducts

--- WebBriliFresh/WebBriliFresh/Models/DiscountProduct.cs
+++ WebBriliFresh/WebBriliFresh/Models/DiscountProduct.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace WebBriliFresh.Models;
 
@@ -9,12 +10,16 @@ public partial class DiscountProduct
 
     public int? ProId { get; set; }
 
+    [DisplayName("Tỉ lệ giảm")]
     public double? Value { get; set; }
 
+    [DisplayName("Bắt đầu")]
     public DateTime? StartDate { get; set; }
 
+    [DisplayName("Kết thúc")]
     public DateTime? EndDate { get; set; }
 
+    [DisplayName("Trạng thái")]
     public bool? Status { get; set; }
 
     public virtual Product? Pro { get; set; }
