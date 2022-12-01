ddd4abb278c817ca06755d1522e7e00f4e2a1940
phuongvt24 <101643872+phuongvt24@users.noreply.github.com>
2022-12-01 18:40:17 +0700

    create image

--- WebBriliFresh/WebBriliFresh/Areas/Admin/Controllers/AdminProductsController.cs
+++ WebBriliFresh/WebBriliFresh/Areas/Admin/Controllers/AdminProductsController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.Build.Framework;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using WebBriliFresh.Models;
 using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
@@ -19,17 +20,19 @@ namespace WebBriliFresh.Areas.Admin.Controllers
     public class AdminProductsController : Controller
     {
         private readonly BriliFreshDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
         public INotyfService _notifyService { get; }
-        public AdminProductsController(BriliFreshDbContext context, INotyfService notifyService)
+        public AdminProductsController(BriliFreshDbContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
-            _notifyService = notifyService;
+            this._hostEnvironment = hostEnvironment;
+
         }
 
         // GET: Admin/AdminProducts
         public async Task<IActionResult> Index()
         {
-            var products = _context.Products.Include(s => s.Type).Where(x => x.IsDeleted == 0);
+            var products = _context.Products.Include(s => s.Type).Include(p => p.ProductImages).Include(a=>a.Stocks).Where(x => x.IsDeleted == 0);
             return View(await products.ToListAsync());
         }
 
@@ -77,17 +80,67 @@ namespace WebBriliFresh.Areas.Admin.Controllers
                 product.IsDeleted = 0;
                 _context.Add(product);
                 await _context.SaveChangesAsync();
+                //return RedirectToAction("UploadPhoto", product);
+
+
+
+                if (product.File != null)
+                {
+                    string wwwRootPath = _hostEnvironment.WebRootPath;
+                    string file1 = Path.GetFileNameWithoutExtension(Path.GetRandomFileName())
+                               + Path.GetExtension(product.File.FileName);
+                    string file2 = Path.Combine(wwwRootPath + "/ImageProduct/", file1);
+
+                    using (var filesream = new FileStream(file2, FileMode.Create))
+                    {
+                        await product.File.CopyToAsync(filesream);
+                    }
+
+                    _context.Add(new ProductImage()
+                    {
+                        ProId = product.ProId,
+                        ImgData = file1
+                    });
+                    await _context.SaveChangesAsync();
+                }
+
+                if (product.Files != null)
+                {
+                    string wwwRootPath = _hostEnvironment.WebRootPath;
+                    foreach (var file in product.Files)
+                    {
+                        string file1 = Path.GetFileNameWithoutExtension(Path.GetRandomFileName())
+                               + Path.GetExtension(file.FileName);
+                        string file2 = Path.Combine(wwwRootPath + "/ImageProduct/", file1);
+
+                        using (var filesream = new FileStream(file2, FileMode.Create))
+                        {
+                            await file.CopyToAsync(filesream);
+                        }
+
+                        _context.Add(new ProductImage()
+                        {
+                            ProId = product.ProId,
+                            ImgData = file1
+                        });
+                        await _context.SaveChangesAsync();
+                    }
+
+                }
+
                 return RedirectToAction(nameof(Index));
+
             }
-            ViewData["TypeId"] = new SelectList(_context.Types, "TypeId", "TypeId", product.TypeId);
-            ViewData["TypeId1"] = new SelectList(_context.Types.Where(x => x.MainType == "Rau củ"), "TypeId", "SubType");
-            ViewData["TypeId2"] = new SelectList(_context.Types.Where(x => x.MainType == "Thịt cá"), "TypeId", "SubType");
-            ViewData["TypeId3"] = new SelectList(_context.Types.Where(x => x.MainType == "Trái cây 4 mùa"), "TypeId", "SubType");
+            else
+            {
+                ViewData["TypeId"] = new SelectList(_context.Types, "TypeId", "TypeId", product.TypeId);
+                ViewData["TypeId1"] = new SelectList(_context.Types.Where(x => x.MainType == "Rau củ"), "TypeId", "SubType");
+                ViewData["TypeId2"] = new SelectList(_context.Types.Where(x => x.MainType == "Thịt cá"), "TypeId", "SubType");
+                ViewData["TypeId3"] = new SelectList(_context.Types.Where(x => x.MainType == "Trái cây 4 mùa"), "TypeId", "SubType"); ViewData["MainType"] = new SelectList(_context.Types.GroupBy(p => p.MainType).Select(x => new { MainType = x.Key }), "MainType", "MainType");
+                return View();
+            }
+            
 
-
-            ViewData["MainType"] = new SelectList(_context.Types.GroupBy(p => p.MainType)
-                                                                .Select(x => new { MainType = x.Key }), "MainType", "MainType");
-            return  View();
         }
 
         // GET: Admin/AdminProducts/Edit/5
@@ -103,6 +156,10 @@ namespace WebBriliFresh.Areas.Admin.Controllers
             {
                 return NotFound();
             }
+            ViewData["TypeId1"] = new SelectList(_context.Types.Where(x => x.MainType == "Rau củ"), "TypeId", "SubType");
+            ViewData["TypeId2"] = new SelectList(_context.Types.Where(x => x.MainType == "Thịt cá"), "TypeId", "SubType");
+            ViewData["TypeId3"] = new SelectList(_context.Types.Where(x => x.MainType == "Trái cây 4 mùa"), "TypeId", "SubType");
+            ViewData["MainType"] = new SelectList(_context.Types.GroupBy(p => p.MainType).Select(x => new { MainType = x.Key }), "MainType", "MainType");
             ViewData["TypeId"] = new SelectList(_context.Types, "TypeId", "TypeId", product.TypeId);
             return View(product);
         }
@@ -112,6 +169,7 @@ namespace WebBriliFresh.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        
         public async Task<IActionResult> Edit(int id, [Bind("ProId,ProName,Price,TypeId,Source,StartDate,Des,Unit,IsDeleted")] Product product)
         {
             if (id != product.ProId)
@@ -137,6 +195,7 @@ namespace WebBriliFresh.Areas.Admin.Controllers
                         throw;
                     }
                 }
+                
                 return RedirectToAction(nameof(Index));
             }
             ViewData["TypeId"] = new SelectList(_context.Types, "TypeId", "TypeId", product.TypeId);
@@ -187,77 +246,61 @@ namespace WebBriliFresh.Areas.Admin.Controllers
         }
 
 
-        public class UploadListFile
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> UploadPhoto(Product product)
         {
-            [Required(ErrorMessage = "Hãy chọn tệp hình ảnh để tải!")]
-            [DataType(DataType.Upload)]
-            [FileExtensions(Extensions ="png,jpg,jpeg,gif")]
-            [Display(Name = "Chọn tệp để tải")]
-            public List<IFormFile>? Files { get; set; }
-            
+            var pro = _context.Products.Where(e => e.ProId == product.ProId);
+            if (pro == null)
+            {
+                return NotFound("Không có sản phẩm");
+            }
+            ViewData["product"] = pro;
+
+            if (product.File != null)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                var file1 = Path.GetFileNameWithoutExtension(Path.GetRandomFileName())
+                           + Path.GetExtension(product.File.FileName);
+                var file2 = Path.Combine(wwwRootPath + "/ImageProduct/", file1);
+
+                using (var filesream = new FileStream(file2, FileMode.Create))
+                {
+                    await product.File.CopyToAsync(filesream);
+                }
+
+                _context.Add(new ProductImage()
+                {
+                    ProId = product.ProId,
+                    ImgData = file1
+                });
+                await _context.SaveChangesAsync();
+            }
+
+            if (product.Files != null)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                for(int i = 0; i<= product.Files.Count;i++ ) 
+                {
+                    var file1 = Path.GetFileNameWithoutExtension(Path.GetRandomFileName())
+                           + Path.GetExtension(product.Files[i].FileName);
+                    var file2 = Path.Combine(wwwRootPath + "/ImageProduct/", file1);
+
+                    using (var filesream = new FileStream(file2, FileMode.Create))
+                    {
+                        await product.Files[i].CopyToAsync(filesream);
+                    }
+
+                    _context.Add(new ProductImage()
+                    {
+                        ProId = product.ProId,
+                        ImgData = file1
+                    });
+                    await _context.SaveChangesAsync();
+                }
+
+            }
+            return View(nameof(Index));
         }
-
-
-    //    [HttpGet]
-    //    public IActionResult UploadPhoto(int id)
-    //    {
-    //        var product = _context.Products.Where(e => e.ProId == id)
-    //                                        .Include(p => p.ProductImages)
-    //                                        .FirstOrDefault();
-    //        if (product == null)
-    //        {
-    //            return NotFound("Không có sản phẩm");
-    //        }
-    //        ViewData["product"] = product;  
-
-    //        return View(new UploadListFile());
-    //    }
-
-    //    [HttpPost, ActionName("UploadPhoto")]
-    //    public async Task< IActionResult> UploadPhotoAsync(int id, UploadListFile lf)
-    //    {
-    //        var product = _context.Products.Where(e => e.ProId == id)
-    //                                        .Include(p=>p.ProductImages)
-    //                                        .FirstOrDefault();
-    //        if (product == null)
-    //        {
-    //            return NotFound("Không có sản phẩm  akhd ádks");
-    //        }
-    //        ViewData["product"] = product;
-
-
-    //        foreach (var formFile in lf)
-    //        {
-    //            if (formFile.Length > 0)
-    //            {
-    //                using (var stream = new FileStream(filePath, FileMode.Create))
-    //                {
-    //                    await formFile.CopyToAsync(stream);
-    //                }
-    //            }
-    //        }
-
-
-
-    //        if (f != null)
-    //        {
-    //            var file1 = Path.GetFileNameWithoutExtension(Path.GetRandomFileName())
-    //                       + Path.GetExtension(f.Files.FileName);
-    //            var file2 = Path.Combine("Uploads","Images","ProductImages",file1);
-
-    //            using (var filesream = new FileStream(file2, FileMode.Create))
-    //            {
-    //                await f.Files.CopyToAsync(filesream);
-    //            }
-
-    //            _context.Add(new ProductImage()
-    //            {
-    //                ProId = product.ProId,
-    //                ImgData = file1
-    //            });
-    //            await _context.SaveChangesAsync();
-    //        }
-    //        return View(new UploadOneFile());
-    //    }
     }
 }
