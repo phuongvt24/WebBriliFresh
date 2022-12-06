8531e7db3039043a74e8c22a19f6fbc875475287
quynhchi <doquynhchi1311@gmail.com>
2022-12-06 22:53:36 +0800

    Squashed commit of the following:
    commit ff05df6b0f46cdbdf61b6a6be126fd8bc131ff5b
    Merge: a4a6b80 a05d50f
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Sat Dec 3 09:07:57 2022 +0700
    
        Merge remote-tracking branch 'origin/feature/mergeFromDev' into feature/DiscountProduct
    
    commit a4a6b8027ca2e54947a0ccd55d0359a4049e2175
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Fri Dec 2 02:53:49 2022 +0800
    
        Add date validation & copy display from DiscountOrder
    
    commit 0b011bf59c65ee08e264a3bf005ebc31a72e1f05
    Merge: 8f41041 b679d68
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Thu Dec 1 01:31:42 2022 +0700
    
        Merge branch 'develop' into feature/DiscountProduct
    
    commit 8f410418889948693fba2a8fdc0ac2c52d8dca25
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Wed Nov 30 02:23:36 2022 +0800
    
        Redirect for button 'Edit', 'Delete' & display ProName in view
    
    commit a04674c8d7724c2b4a6494b0af47f21415577b5d
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Wed Nov 30 01:58:49 2022 +0800
    
        Update _SideNavPartialView.cshtml
    
    commit cbc81675d921532656be4102e5fceff708114bd5
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Wed Nov 30 01:53:53 2022 +0800
    
        Scaffold CRUD for DiscountProducts

---
+++ WebBriliFresh/WebBriliFresh/Areas/Admin/Controllers/DiscountProductsController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebBriliFresh.Models;
+
+namespace WebBriliFresh.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DiscountProductsController : Controller
+    {
+        private readonly BriliFreshDbContext _context;
+
+        public DiscountProductsController(BriliFreshDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/DiscountProducts
+        public async Task<IActionResult> Index()
+        {
+            var briliFreshDbContext = _context.DiscountProducts.Include(d => d.Pro);
+            return View(await briliFreshDbContext.ToListAsync());
+        }
+
+        // GET: Admin/DiscountProducts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.DiscountProducts == null)
+            {
+                return NotFound();
+            }
+
+            var discountProduct = await _context.DiscountProducts
+                .Include(d => d.Pro)
+                .FirstOrDefaultAsync(m => m.DisId == id);
+            if (discountProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(discountProduct);
+        }
+
+        // GET: Admin/DiscountProducts/Create
+        public IActionResult Create()
+        {
+            ViewData["ProId"] = new SelectList(_context.Products, "ProId", "ProName");
+            return View();
+        }
+
+        // POST: Admin/DiscountProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DisId,ProId,Value,StartDate,EndDate,Status")] DiscountProduct discountProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(discountProduct);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProId"] = new SelectList(_context.Products, "ProId", "ProName", discountProduct.ProId);
+            return View(discountProduct);
+        }
+
+        // GET: Admin/DiscountProducts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.DiscountProducts == null)
+            {
+                return NotFound();
+            }
+
+            var discountProduct = await _context.DiscountProducts.FindAsync(id);
+            if (discountProduct == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProId"] = new SelectList(_context.Products, "ProId", "ProName", discountProduct.ProId);
+            return View(discountProduct);
+        }
+
+        // POST: Admin/DiscountProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DisId,ProId,Value,StartDate,EndDate,Status")] DiscountProduct discountProduct)
+        {
+            if (id != discountProduct.DisId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(discountProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiscountProductExists(discountProduct.DisId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProId"] = new SelectList(_context.Products, "ProId", "ProName", discountProduct.ProId);
+            return View(discountProduct);
+        }
+
+        // GET: Admin/DiscountProducts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.DiscountProducts == null)
+            {
+                return NotFound();
+            }
+
+            var discountProduct = await _context.DiscountProducts
+                .Include(d => d.Pro)
+                .FirstOrDefaultAsync(m => m.DisId == id);
+            if (discountProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(discountProduct);
+        }
+
+        // POST: Admin/DiscountProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.DiscountProducts == null)
+            {
+                return Problem("Entity set 'BriliFreshDbContext.DiscountProducts'  is null.");
+            }
+            var discountProduct = await _context.DiscountProducts.FindAsync(id);
+            if (discountProduct != null)
+            {
+                _context.DiscountProducts.Remove(discountProduct);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiscountProductExists(int id)
+        {
+          return _context.DiscountProducts.Any(e => e.DisId == id);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public IActionResult VerifyDate(DateTime? startDate, DateTime? endDate)
+        {
+            if (startDate != null && endDate != null)
+            {
+                DateTime StartDate = startDate ?? DateTime.MinValue;
+                DateTime EndDate = endDate ?? DateTime.MinValue;
+                int result = DateTime.Compare(StartDate, EndDate);
+                if (result >= 0)
+                {
+                    return Json(false);
+                }
+
+            }
+
+            return Json(true);
+        }
+
+    }
+}
