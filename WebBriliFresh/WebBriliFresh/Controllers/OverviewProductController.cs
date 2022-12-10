1ff14164db012754799d7cf9d7c8cfb5d45e9e85
phuongvt24 <101643872+phuongvt24@users.noreply.github.com>
2022-12-10 11:36:02 +0700

    commit set up layout

---
+++ WebBriliFresh/WebBriliFresh/Controllers/OverviewProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBriliFresh.Controllers
+{
+    public class OverviewProductController : Controller
+    {
+        public IActionResult FishAndMeat()
+        {
+            return View();
+        }
+        public IActionResult Fruit()
+        {
+            return View();
+        }
+
+        public IActionResult Vegetable()
+        {
+            return View();
+        }
+
+        public IActionResult ListFishAndMeat()
+        {
+            return View();
+        }
+        public IActionResult ListFruit()
+        {
+            return View();
+        }
+
+        public IActionResult ListVegetable()
+        {
+            return View();
+        }
+
+
+    }
+}
