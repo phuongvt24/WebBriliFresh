1b2bbff2d5e77290936865de82e9b4cbd66bf187
minhtun64uit <minhtuandexom@gmail.com>
2022-12-14 02:13:22 +0700

    Commit 14/12

--- WebBriliFresh/WebBriliFresh/Models/BriliFreshDbContext.cs
+++ WebBriliFresh/WebBriliFresh/Models/BriliFreshDbContext.cs
@@ -63,7 +63,7 @@ public partial class BriliFreshDbContext : IdentityDbContext<User, ApplicationRo
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=34.142.200.143;Database=BriliFreshDB;user id=brilifreshdb;password=brilifreshdb;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=34.142.200.143;Database=BriliFreshDB;user id=brilifreshdb;password=brilifreshdb;TrustServerCertificate=True;MultipleActiveResultSets=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
