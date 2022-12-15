c4738a962e9e2186619d6a182a0cb5c3b835a546
quynhchi <doquynhchi1311@gmail.com>
2022-12-15 09:32:49 +0700

    Squashed commit of the following:
    commit 59f23aa444a9a880be69485ffd3e4ffbb9519260
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Thu Dec 15 09:30:25 2022 +0700
    
        Adjust view Login in sign-in.css and AccountInfo in index.cshtml
    
    commit 75b9eb7debaad7ea5fdd52621296898046de7da6
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Thu Dec 15 06:15:09 2022 +0800
    
        side bar avatar
    
    commit e216e338230bbf208fc69d6537842a27f47fe46f
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Thu Dec 15 05:51:47 2022 +0800
    
        fix minor bug
    
    commit b1107deefb9c4311a81048917f3fa971f55a7bc3
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Thu Dec 15 05:30:31 2022 +0800
    
        delete old avatar
    
    commit a35c08f2e70e47f7507b52e160cebc58c0ab623f
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Thu Dec 15 05:13:08 2022 +0800
    
        display avatar using session
    
    commit 61523f0d339f666ced1c6747dbf8f25e74421c9a
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Thu Dec 15 05:02:40 2022 +0800
    
        Upload photo
    
    commit 812ad915730e34ab5d6dfcaa77e1c71cf4128422
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Thu Dec 15 00:59:49 2022 +0800
    
        add real name to home navbar
    
    commit 3aed1588f26ec90bb6cba97ca1bf7679ccb5a83d
    Author: quynhchi <doquynhchi1311@gmail.com>
    Date:   Wed Dec 14 23:58:18 2022 +0800
    
        load customer and display info in Thông tin tài khoản

--- WebBriliFresh/WebBriliFresh/Controllers/MyAccountController.cs
+++ WebBriliFresh/WebBriliFresh/Controllers/MyAccountController.cs
@@ -1,12 +1,111 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebBriliFresh.Models;
 
 namespace WebBriliFresh.Controllers
 {
     public class MyAccountController : Controller
     {
+        private readonly BriliFreshDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public MyAccountController(BriliFreshDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        [Authorize(Policy = "LoggedIn")]
         public IActionResult AccountInfo()
         {
-            return View();
+            int cusID = (int)HttpContext.Session.GetInt32("CUS_SESSION_CUSID");
+
+            Customer currentCustomer = _context.Customers.FirstOrDefault(x => x.CusId == cusID);
+
+            return View(currentCustomer);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditAccount(IFormFile photo)
+        {
+
+
+            IFormCollection form = HttpContext.Request.Form;
+            int CusId = Int32.Parse(form["CusId"]);
+            int UserId = Int32.Parse(form["UserId"]);
+            int RewardId = Int32.Parse(form["RewardId"]);
+            int Gender = Int32.Parse(form["Gender"]);
+
+            string FirstName = form["FirstName"].ToString().Trim();
+            string LastName = form["LastName"].ToString().Trim();
+            string Phone = form["Phone"].ToString().Trim();
+            string Email = form["Email"].ToString().Trim();
+
+            try
+            {
+                Customer customer = new Customer();
+                customer.CusId = CusId;
+                customer.UserId = UserId;
+                customer.Email = Email;
+                customer.RewardId = RewardId;
+                customer.FirstName = FirstName;
+                customer.LastName = LastName;
+                customer.RewardId = RewardId;
+                customer.Gender = Gender;
+                customer.Phone = Phone;
+
+                String name = LastName + " " + FirstName;
+                HttpContext.Session.SetString("CUS_SESSION_CUSNAME", name);
+                _context.Update(customer);
+
+                if (photo != null)
+                {
+                    string picfilename = DoPhotoUpload(photo);
+                    User user = await _context.Users.FindAsync(UserId);
+
+                    if(user.Avatar != null)
+                    {
+                        DeleteOldAvatar(user.Avatar);
+                    }
+
+                    user.Avatar = picfilename;
+                    if (await TryUpdateModelAsync<User>(
+                        user,
+                        "user",
+                        s => s.Avatar))
+                    {
+                        // EF will detect the change and update only the column that has changed.
+                        await _context.SaveChangesAsync();
+                        HttpContext.Session.SetString("CUS_SESSION_AVATAR", picfilename);
+                    }
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(AccountInfo));
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(CusId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.CusId == id);
         }
 
         public IActionResult MyNotice()
@@ -29,7 +128,7 @@ namespace WebBriliFresh.Controllers
         {
             return View();
         }
-      
+
         public IActionResult ForgetPass_1()
         {
             return View();
@@ -62,5 +161,38 @@ namespace WebBriliFresh.Controllers
             return View();
         }
 
+
+        private void DeleteOldAvatar(string fname)
+        {
+            try
+            {
+                string fullpath = Path.Combine(_env.WebRootPath, "MyAccountAssets/UserPhotos/" + fname);
+                // Check if file exists with its full path    
+                if (System.IO.File.Exists(fullpath))
+                {
+                    // If file found, delete it    
+                    System.IO.File.Delete(Path.Combine(fullpath));
+                    Console.WriteLine("File deleted.");
+                }
+                else Console.WriteLine("File not found");
+            }
+            catch (IOException ioExp)
+            {
+                Console.WriteLine(ioExp.Message);
+            }
+        }
+
+        private string DoPhotoUpload(IFormFile photo)
+        {
+            string fext = Path.GetExtension(photo.FileName);
+            string uname = Guid.NewGuid().ToString();
+            string fname = uname + fext;
+            string fullpath = Path.Combine(_env.WebRootPath, "MyAccountAssets/UserPhotos/" + fname);
+            using (FileStream fs = new(fullpath, FileMode.Create))
+            {
+                photo.CopyTo(fs);
+            }
+            return fname;
+        }
     }
 }
